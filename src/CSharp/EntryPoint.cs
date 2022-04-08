bf5be353a19816afe2b668889f196a6d3de10c7c
Clément Pit-Claudel <cpitcla@amazon.com>
2022-04-08 15:35:43 -0700

    Initial import

---
+++ src/CSharp/EntryPoint.cs
@@ -0,0 +1,40 @@
+using System.Collections.ObjectModel;
+using DafnyInDafny.CSharp;
+
+namespace Microsoft.Dafny.Compilers.SelfHosting.CSharp;
+
+internal class SelfHostingCSharpCompiler : Plugins.Compiler {
+  private readonly DafnyCSharpCompiler compiler = new ();
+
+  public override IReadOnlySet<string> SupportedExtensions => new HashSet<string> { ".cs", ".dll" };
+
+  public override string TargetLanguage => "C#";
+  public override string TargetExtension => "cs";
+
+  public override string PublicIdProtect(string name) {
+    return new CsharpCompiler().PublicIdProtect(name);
+  }
+
+  public override bool TextualTargetIsExecutable => false;
+  public override bool SupportsInMemoryCompilation => false;
+
+  public override void Compile(Program dafnyProgram, ConcreteSyntaxTree output) {
+    compiler.Compile(dafnyProgram, output);
+  }
+
+  public override void EmitCallToMain(Method mainMethod, string baseName, ConcreteSyntaxTree output) {
+    compiler.EmitCallToMain(mainMethod, baseName, output);
+  }
+
+  public override bool CompileTargetProgram(string dafnyProgramName, string targetProgramText, string callToMain,
+    string pathsFilename, ReadOnlyCollection<string> otherFileNames, bool runAfterCompile, TextWriter outputWriter,
+    out object compilationResult) {
+    throw new NotImplementedException();
+  }
+
+  public override bool RunTargetProgram(string dafnyProgramName, string targetProgramText, string callToMain,
+    string pathsFilename, ReadOnlyCollection<string> otherFileNames, object compilationResult,
+    TextWriter outputWriter) {
+    throw new NotImplementedException();
+  }
+}
